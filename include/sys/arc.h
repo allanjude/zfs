3c3a8396483db820cac0802aa2977a8b5f5090ac
Allan Jude <allanjude@freebsd.org>
2019-05-31 03:33:30 +0000

    fix arc_alloc_raw_buf() prototype

--- include/sys/arc.h
+++ include/sys/arc.h
@@ -250,7 +250,7 @@ arc_buf_t *arc_alloc_compressed_buf(spa_t *spa, void *tag,
 arc_buf_t *arc_alloc_raw_buf(spa_t *spa, void *tag, uint64_t dsobj,
     boolean_t byteorder, const uint8_t *salt, const uint8_t *iv,
     const uint8_t *mac, dmu_object_type_t ot, uint64_t psize, uint64_t lsize,
-    enum zio_compress compression_type);
+    enum zio_compress compression_type, int8_t complevel);
 int8_t arc_get_complevel(arc_buf_t *buf);
 arc_buf_t *arc_loan_buf(spa_t *spa, boolean_t is_metadata, int size);
 arc_buf_t *arc_loan_compressed_buf(spa_t *spa, uint64_t psize, uint64_t lsize,
