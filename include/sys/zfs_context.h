9f5133052712a304b88f2afbd2dcc092742f2bbe
Allan Jude <allanjude@freebsd.org>
2019-06-06 01:59:06 +0000

    Define KM_ZERO for userspace

--- include/sys/zfs_context.h
+++ include/sys/zfs_context.h
@@ -393,6 +393,7 @@ void procfs_list_add(procfs_list_t *procfs_list, void *p);
 #define	KM_SLEEP		UMEM_NOFAIL
 #define	KM_PUSHPAGE		KM_SLEEP
 #define	KM_NOSLEEP		UMEM_DEFAULT
+#define	KM_ZERO			0	/* supposed to zero the allocation, ignore for userspace */
 #define	KM_NORMALPRI		0	/* not needed with UMEM_DEFAULT */
 #define	KMC_NODEBUG		UMC_NODEBUG
 #define	KMC_KMEM		0x0
