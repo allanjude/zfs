1f0c802c95672ed2497ffb3cda0d03d8ec7e7a79
Allan Jude <allanjude@freebsd.org>
2019-06-06 00:59:15 +0000

    Fix prototype

--- include/sys/zio_compress.h
+++ include/sys/zio_compress.h
@@ -175,7 +175,7 @@ extern size_t lz4_compress_zfs(void *src, void *dst, size_t s_len, size_t d_len,
 extern int lz4_decompress_zfs(void *src, void *dst, size_t s_len, size_t d_len,
     int level);
 
-extern void zstd_init(void);
+extern int zstd_init(void);
 extern void zstd_fini(void);
 extern size_t zstd_compress(void *src, void *dst, size_t s_len, size_t d_len,
     int level);
