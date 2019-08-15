af6d25c83db7e5e5f8d96937f3a2534cd7e05613
Allan Jude <allanjude@freebsd.org>
2019-08-15 01:36:13 +0000

    Work around integer size mismatch with zap_length();
    zap_length()'s integer_size argument is uint64_t
    However, zap_attribute_t's za_integer_length is an int (int8_t)
    
    Cheat and use za_first_integer to avoid another stack allocation
    
    Signed-off-by: Allan Jude <allanjude@freebsd.org>

--- module/zfs/vdev.c
+++ module/zfs/vdev.c
@@ -5101,10 +5101,17 @@ vdev_prop_get(vdev_t *vd, nvlist_t *innvl, nvlist_t *outnvl)
 				break;
 			}
 
+			/*
+			 * za.za_integer_length is an int, but zap_length()
+			 * expects a uint64_t. Use za_first_integer instead.
+			 */
 			err = zap_length(mos, objid, nvpair_name(elem),
-			    &za.za_integer_length, &za.za_num_integers);
+			    &za.za_first_integer, &za.za_num_integers);
 			if (err)
 				break;
+
+			za.za_integer_length = za.za_first_integer;
+			za.za_first_integer = NULL;
 
 			switch (za.za_integer_length) {
 			case 8:
